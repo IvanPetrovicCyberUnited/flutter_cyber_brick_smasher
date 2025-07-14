81f6dbde6071c15ad0d25811afb104c7e78cfc46
Ivan Petrovic <ivan.petrovic@cyberunited.at>
2025-07-14 17:27:27 +0200

    Add ball image above paddle

---
+++ lib/game_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class GameScreen extends StatelessWidget {
+  const GameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: Image.asset('assets/images/paddle.png'),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 56.0),
+              child: Image.asset('assets/images/ball.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
